13e77b327f80dbce65ffd66ab50946cea0ab8468
williamSabino <96441509+williamSabino@users.noreply.github.com>
2024-04-10 15:14:34 -0300

    init
    initial-commit

---
+++ BlocoDeNotas/src/main/java/br/com/will/BlocoDeNotas/BlocoDeNotasApplication.java
@@ -0,0 +1,13 @@
+package br.com.will.BlocoDeNotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlocoDeNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlocoDeNotasApplication.class, args);
+	}
+
+}
---
+++ BlocoDeNotas/src/test/java/br/com/will/BlocoDeNotas/BlocoDeNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.will.BlocoDeNotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlocoDeNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
